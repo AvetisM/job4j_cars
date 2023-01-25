e4d4a8b7340ed93f9c1811f223bbcaf718811c6b
AMkhitarants <avetis.mkhitaryants@gmail.com>
2023-01-25 10:03:45 +0300

    2. HQL [#6874]

--- src/main/java/ru/job4j/cars/model/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/model/repository/UserRepository.java
@@ -24,6 +24,7 @@ public class UserRepository {
     public User create(User user) {
         Session session = sf.openSession();
         try {
+            session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
         } catch (Exception e) {
